ed0a313005bb16e1760d4e91ceb6d8518dd9501a
yoohoon81 <yoohoony@hotmail.com>
2017-10-10 02:07:34 +1100

    Delete MessageFromSchedulingAgentToCar.java

--- MessageFromSchedulingAgentToCar.java
+++
@@ -1,9 +0,0 @@
-package ScheduleSystem;
-
-import java.util.Date;
-
-public class MessageFromSchedulingAgentToCar {
-	public int BayNumber; // Bay the car should come to recharge
-	public Date StartTime; // booked check-in time
-	public Date FinishTime; // booked check-out time
-}
